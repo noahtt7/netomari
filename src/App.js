ecacc3263be3f216856fb9cc7180c5ef727069bb
noahtt7 <noahtaguma@gmail.com>
2024-10-17 09:51:57 -0700

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Netomari</h1>
+      <Navbar/>
+    </div>
+  );
+}
+
+export default App;
